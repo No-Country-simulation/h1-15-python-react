318cc9f5685838b9d6fc4e4f487c3009fdd14cf0
rafaric <rafaelstrongoli@gmail.com>
2024-07-08 20:25:41 -0300

    configuracion inicial.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+      <section className="flex justify-center w-screen bg-gradient-to-b from-violet-600 via-violet-600 to-indigo-600 flex-col items-center h-screen gap-20">
+        <h1 className="text-red-500 text-2xl font-bold font-merrywether">
+          PROXIMAMENTE
+        </h1>
+        <img src="/justinaLogo.png" alt="logo" />
+
+        <p className="text-yellow-400 text-2xl font-semibold text-center font-lato">
+          Acercando a los donantes y simplificando el proceso medico
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
