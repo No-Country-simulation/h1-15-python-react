99320c0982fe4649cae92c56dff6cd5fac11dea9
Fernando <fer.gab.sua@gmail.com>
2024-07-22 10:02:11 -0300

    1.0

--- Backend/financiadores/serializers.py
+++ Backend/financiadores/serializers.py
@@ -1,7 +1,24 @@
+
 from rest_framework import serializers
-from core.models import Financiador
+from core.models import Financiador, FileUpload
+import os
 
 class FinanciadorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Financiador
         fields = '__all__'
+
+
+
+class FileUploadSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FileUpload
+        fields = ('id', 'file', 'uploaded_at')
+
+        def validate_file(self, value):
+            print("Validating file:", value.name)  # Añade esta línea
+            ext = os.path.splitext(value.name)[1]
+            valid_extensions = ['.xlsx', '.xls']
+            if ext.lower() not in valid_extensions:
+                raise serializers.ValidationError('Unsupported file extension. Allowed extensions are .xlsx, .xls')
+            return value
--- Backend/financiadores/views.py
+++ Backend/financiadores/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from rest_framework import generics
-from core.models import Financiador
-from financiadores.serializers import FinanciadorSerializer
+from rest_framework import generics, status
+from rest_framework.views import APIView
+from core.models import Financiador , FileUpload
+from rest_framework.response import Response
+from financiadores.serializers import FinanciadorSerializer, FileUploadSerializer
+from rest_framework.parsers import MultiPartParser, FormParser
 from drf_spectacular.utils import extend_schema
 # Create your views here.
 
@@ -24,3 +27,46 @@ class FinanciadoresList(generics.ListCreateAPIView):
     )
     def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
+
+
+class FinanciadoresDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Financiador.objects.all()
+    serializer_class = FinanciadorSerializer
+
+    @extend_schema(
+        tags=['financiador'],
+        summary='Lista un financiador especifico por id',
+        description="Entrega un financiador especificado con su numero de ID"
+    )
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    @extend_schema(
+        tags=['financiador'],
+        summary='Modifica un financiador',
+        description="Permite actualizar todos los datos de un financiador especificado con su numero de ID"
+    )
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    @extend_schema(
+        tags=['financiador'],
+        summary='Elimina un financiador',
+        description="Elimina de la base de datos el financiador especificado con su numero de ID"
+    )
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+
+class FileUploadView(APIView):
+    parser_classes = (MultiPartParser, FormParser)
+
+
+    def post(self, request, *args, **kwargs):
+        file_serializer = FileUploadSerializer(data=request.data)
+        if file_serializer.is_valid():
+            file_serializer.save()
+            print("ACA SI")
+            return Response(file_serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
