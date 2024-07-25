58f893929a39fdf217fcb280d14109b4642e5f7e
maxcerecedadev <cereceda1991@gmail.com>
2024-07-25 16:27:50 -0500

    feat: add styles and filter card events

--- Frontend/src/components/SearchBar/SearchBar.jsx
+++ Frontend/src/components/SearchBar/SearchBar.jsx
@@ -1,12 +1,13 @@
-import { useState } from 'react';
-import Icon from '../Icon/Icon';
+import { useState, useEffect } from "react";
+import Icon from "../Icon/Icon";
 
-const SearchBar = () => {
+// eslint-disable-next-line react/prop-types
+const SearchBar = ({ onFocus, onBlur }) => {
   const [isInputVisible, setInputVisible] = useState(false);
-  const [searchValue, setSearchValue] = useState('');
+  const [searchValue, setSearchValue] = useState("");
 
   const handleSearchClick = () => {
-    if (isInputVisible && searchValue.trim() !== '') {
+    if (isInputVisible && searchValue.trim() !== "") {
       handleSearchSubmit();
     } else {
       setInputVisible(!isInputVisible);
@@ -18,21 +19,34 @@ const SearchBar = () => {
   };
 
   const handleSearchSubmit = () => {
-    if (searchValue.trim() !== '') {
-      console.log('Searching for:', searchValue);
-      setSearchValue(''); 
-      setInputVisible(false); 
+    if (searchValue.trim() !== "") {
+      console.log("Searching for:", searchValue);
+      setSearchValue("");
+      setInputVisible(false);
     }
   };
 
+  useEffect(() => {
+    if (onFocus && isInputVisible) {
+      onFocus();
+    }
+    else if (onBlur && !isInputVisible) {
+      onBlur();
+    }
+  }, [isInputVisible, onFocus, onBlur]);
+
   return (
-    <div className="flex items-center space-x-2">
+    <div
+      className={`flex items-center space-x-2 ${
+        isInputVisible ? "w-full" : ""
+      }`}
+    >
       {isInputVisible && (
         <input
           type="text"
           value={searchValue}
           onChange={handleInputChange}
-          onKeyDown={(e) => e.key === 'Enter' && handleSearchSubmit()}
+          onKeyDown={(e) => e.key === "Enter" && handleSearchSubmit()}
           className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-secondary-500"
           placeholder="Empezar a buscar..."
         />
