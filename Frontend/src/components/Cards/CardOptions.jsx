afc2ffc68f262429c945cad435a9191cbc8520f4
maxcerecedadev <cereceda1991@gmail.com>
2024-07-25 18:12:08 -0500

    feat: add link to card in main patient

--- Frontend/src/components/Cards/CardOptions.jsx
+++ Frontend/src/components/Cards/CardOptions.jsx
@@ -1,18 +1,18 @@
+import { Link } from "react-router-dom";
+
 /* eslint-disable react/prop-types */
-const CardOptions = ({ imagen, titulo, color }) => {
+const CardOptions = ({ imagen, titulo, color, link }) => {
   return (
-    <div
-      className={`${color} flex flex-col items-center gap-2 rounded-[18px] w-[155px] h-[137px] pt-6 pb-4 px-4`}
-    >
+    <Link to={link} className={`${color} flex flex-col items-center gap-2 rounded-[18px] w-full h-[137px] pt-6 pb-4 px-4`} style={{ textDecoration: 'none' }}>
       <div className="w-[50px] h-[50px] rounded-xl bg-white flex justify-center items-center">
-        <img className="w-[24px] h-[24px]" src={imagen} alt="" />
+        <img className="w-[24px] h-[24px]" src={imagen} alt={titulo} />
       </div>
       <div className="flex items-center h-[38px]">
         <p className="text-center text-white text-base font-semibold leading-5">
           {titulo}
         </p>
       </div>
-    </div>
+    </Link>
   );
 };
 
