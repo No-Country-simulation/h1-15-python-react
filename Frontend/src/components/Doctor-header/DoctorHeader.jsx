5a996c2abf0e8244488999b9d4eebe2a8d0ab41f
LautaroLD <duranlautarogabriel@gmail.com>
2024-07-31 23:32:39 -0300

    update DoctorHeader.jsx and Doctor-layout.jsx

--- Frontend/src/components/Doctor-header/DoctorHeader.jsx
+++ Frontend/src/components/Doctor-header/DoctorHeader.jsx
@@ -1,13 +1,34 @@
-import UserMenu from "../UserMenu";
+import { IoNotifications } from "react-icons/io5";
+import { FaAngleDown } from "react-icons/fa6";
+import Logout from "../Logout/Logout";
+import { useState } from "react";
 
 export default function DoctorHeader() {
+  const [showLogout, setShowLogout] = useState(false);
+  const toggleShowLogout = () => {
+    setShowLogout((prev) => !prev);
+  };
   return (
-    <div className="flex gap-10 justify-end w-full">
-      <div className="">⭕️</div>
-      <div className="">🔔</div>
-      <div>
-        <UserMenu />
+    <div className="flex gap-10 justify-end w-full items-center">
+      <div className="bg-complementary-50 rounded-full p-2 cursor-pointer">
+        <IoNotifications className="text-complementary-500 text-2xl" />
       </div>
+      <div className="flex items-center gap-3 relative">
+        <img src="/ProfileImageSlide.png" className="w-10 aspect-square" />
+        <div>
+          <p>Ashley Lars</p>
+          <p className="text-slate-500">Receptionist</p>
+        </div>
+        <FaAngleDown className="cursor-pointer" onClick={toggleShowLogout} />
+        {showLogout && (
+          <div className="absolute top-full right-0">
+            <Logout />
+          </div>
+        )}
+      </div>
+      {/* <div>
+        <UserMenu />
+      </div> */}
     </div>
   );
 }
