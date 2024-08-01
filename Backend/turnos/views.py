475846db97162a8d6418bc0136b406f2de337ba9
DamBeDev <143622448+DamBeDev@users.noreply.github.com>
2024-07-31 23:16:12 -0300

    Se incluye parte logica de la vista de turnos

--- Backend/turnos/views.py
+++ Backend/turnos/views.py
@@ -1,11 +1,13 @@
 from turnos.serializers import TurnoSerializer, DisponibilidadSerializer
-from rest_framework import generics
+from rest_framework import generics, views
+from rest_framework import status
+from rest_framework.response import Response
 from drf_spectacular.utils import extend_schema
 from core.models import Turno, Disponibilidad
 from custom_functions.date_list import validar_fechas
 
 
-class DisponibilidadListCreate(generics.ListCreateAPIView):
+class DisponibilidadList(generics.ListAPIView):
     queryset = Disponibilidad.objects.all()
     serializer_class = DisponibilidadSerializer
     
@@ -17,16 +19,34 @@ class DisponibilidadListCreate(generics.ListCreateAPIView):
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
 
+
+class DisponibilidadCreate(views.APIView):
     @extend_schema(
         tags=['Disponibilidad'],
         summary='Carga la disponibilidad del médico',
-        description="Carga una nueva disponibilidad para el médico a partir de la cual se generan los turnos"
+        description="""Carga una nueva disponibilidad para el médico a partir de la cual se generan los turnos.\n
+                    Requiere el nombre del médico, de la institución; y día, hora de inicio de turnos, hora de fin de turnos en forma de lista.\n
+                    Formato:\n
+                    {
+                        'medico': 'nombre medico',
+                        'institucion': 'nombre institucion',
+                        'horarios': {
+                            1: ['lunes', '08:00', '12:00'],
+                            2: ['martes', '08:00', '16:00'],
+                            3: ['jueves', '10:00', '17:00'],
+                            },
+                    }
+                    """
     )
-    def post(self, request, *args, **kwargs):
+    def post(self, request):
+        datos_disponibilidad = request.data
+        disponibilidad_serializer = DisponibilidadSerializer(datos_disponibilidad)
         
-        
-        
-        return self.create(request, *args, **kwargs)
+        if disponibilidad_serializer.is_valid():
+            disponibilidad_serializer.save()
+            return Response(disponibilidad_serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(disponibilidad_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class DisponibilidadDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Disponibilidad.objects.all()
