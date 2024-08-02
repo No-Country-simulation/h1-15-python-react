e95b036623e14326b8ee2abeb0089670e3f8b5db
Fernando <fer.gab.sua@gmail.com>
2024-08-02 00:39:38 -0300

    Update common.py

--- Backend/justina_io_server/settings/common.py
+++ Backend/justina_io_server/settings/common.py
@@ -157,10 +157,12 @@ SIMPLE_JWT = {
 }
 
 # Email settings
-EMAIL_HOST = os.environ.get('DJANGO_EMAIL_HOST')
-EMAIL_HOST_USER = os.environ.get('DJANGO_EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('DJANGO_EMAIL_HOST_PASSWORD')
-EMAIL_PORT = os.environ.get('DJANGO_EMAIL_PORT')
+
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_PORT = os.environ.get('EMAIL_PORT')
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 
 SPECTACULAR_SETTINGS = {
