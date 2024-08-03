24fa0cfb4ba4f2ec0de8be44002bc2619450983a
maxcerecedadev <cereceda1991@gmail.com>
2024-08-03 11:45:23 -0500

    feat: add translation on components, login, 404 not found, page under construction, main patient, main patient card options

--- Frontend/src/components/PageConstruction/PageConstruction.jsx
+++ Frontend/src/components/PageConstruction/PageConstruction.jsx
@@ -3,10 +3,10 @@ import BackButton from "../BackButton/BackButton";
 import { showToast } from "../../utils/toast";
 import { ToastContainer } from "react-toastify";
 import { useNavigate } from "react-router-dom";
+import useLanguage from "../../hooks/useLanguage";
 
 const PageConstruction = () => {
   const [email, setEmail] = useState("");
-
   const navigate = useNavigate();
 
   const handleEmailChange = (e) => {
@@ -15,12 +15,18 @@ const PageConstruction = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    showToast("Serás notificado.", "success");
+    showToast(languageData.PageConstruction.toastSuccessMessage, "success");
 
     setTimeout(() => {
       navigate("/patient");
     }, 2000);
   };
+
+  const languageData = useLanguage();
+
+  if (!languageData) {
+    return <div>Cargando datos...</div>;
+  }
 
   return (
     <main>
@@ -28,9 +34,11 @@ const PageConstruction = () => {
       <div className="flex flex-col items-center justify-center min-h-screen p-4 font-sans">
         <div className="text-center mb-4">
           <h1 className="text-2xl font-bold mb-2">
-            ¡Esta sección está en construcción!
+            {languageData.PageConstruction.title}
           </h1>
-          <p className="text-lg mb-4">Notificarme cuando esté disponible</p>
+          <p className="text-lg mb-4">
+            {languageData.PageConstruction.message}
+          </p>
           <form
             onSubmit={handleSubmit}
             className="flex flex-col sm:flex-row items-center"
@@ -39,7 +47,7 @@ const PageConstruction = () => {
               type="email"
               value={email}
               onChange={handleEmailChange}
-              placeholder="Introduce tu correo electrónico"
+              placeholder={languageData.PageConstruction.emailPlaceholder}
               required
               className="px-4 py-2 border rounded-lg outline-none w-full mb-2 sm:mb-0 sm:mr-2"
             />
@@ -47,13 +55,17 @@ const PageConstruction = () => {
               type="submit"
               className="px-4 py-2 bg-Justina_1 text-white font-semibold rounded-lg"
             >
-              Enviar
+              {languageData.PageConstruction.submitButton}
             </button>
           </form>
         </div>
         <hr className="w-full mt-8 border-t-2 border-gray-300" />
         <div className="mt-4">
-          <img src="/justinaLogo.png" alt="Logo" className="w-48 h-auto" />
+          <img
+            src="/justinaLogo.png"
+            alt={languageData.PageConstruction.logoAltText}
+            className="w-48 h-auto"
+          />
         </div>
       </div>
       <ToastContainer />
--- Frontend/src/pages/Login/Login.jsx
+++ Frontend/src/pages/Login/Login.jsx
@@ -4,6 +4,7 @@ import eye_off from "/icons/eye_off.svg";
 import { useNavigate } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import { showToast } from "../../utils/toast";
+import useLanguage from "../../hooks/useLanguage";
 
 const Login = () => {
   const [passwordVisible, setPasswordVisible] = useState(false);
@@ -59,6 +60,12 @@ const Login = () => {
     }
   };
 
+  const languageData = useLanguage();
+
+  if (!languageData) {
+    return <div>Cargando datos...</div>;
+  }
+
   return (
     <main className="flex flex-col items-center justify-center min-h-screen p-4 bg-gradient-background_1">
       <ToastContainer />
@@ -66,20 +73,22 @@ const Login = () => {
         <img onClick={() => navigate("/")} src="/justinaLogo.png" alt="logo" />
       </section>
       <section className="flex flex-col justify-center w-full p-6 bg-white shadow-lg rounded-lg lg:max-w-[548px]">
-        <h1 className="text-2xl font-bold text-center">Iniciar Sesión</h1>
+        <h1 className="text-2xl font-bold text-center">
+          {languageData.login.title}
+        </h1>
         <p className="mt-6 text-center text-sm text-text_primary">
-          ¿No tienes cuenta?
+          {languageData.login.noAccount}
           <span
             onClick={() => navigate("/register")}
             className="ml-1 font-semibold cursor-pointer text-text_secondary"
           >
-            Regístrate
+            {languageData.login.register}
           </span>
         </p>
         <form className="flex flex-col space-y-6 mt-6" onSubmit={handleSubmit}>
           <div>
             <label htmlFor="email" className="block text-sm font-medium">
-              Correo
+              {languageData.login.emailLabel}
             </label>
             <input
               id="email"
@@ -87,13 +96,13 @@ const Login = () => {
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               required
-              placeholder="Ingrese su correo electrónico"
+              placeholder={languageData.login.emailPlaceholder}
               className="w-full mt-1 px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-secondary-500 focus:border-secondary-500 sm:text-sm"
             />
           </div>
           <div>
             <label htmlFor="password" className="block text-sm font-medium">
-              Contraseña
+              {languageData.login.passwordLabel}
             </label>
             <div className="relative">
               <input
@@ -102,7 +111,7 @@ const Login = () => {
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
                 required
-                placeholder="Ingrese una contraseña"
+                placeholder={languageData.login.passwordPlaceholder}
                 className="w-full mt-1 px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-secondary-500 focus:border-secondary-500 sm:text-sm"
               />
               <button
@@ -114,8 +123,8 @@ const Login = () => {
                   src={passwordVisible ? eye_off : eye}
                   alt={
                     passwordVisible
-                      ? "Ocultar contraseña"
-                      : "Mostrar contraseña"
+                      ? languageData.login.eyeAltHide
+                      : languageData.login.eyeAltShow
                   }
                   className="w-4 h-4"
                 />
@@ -132,24 +141,24 @@ const Login = () => {
                 className="w-4 h-4 text-blue-600 border-gray-300 rounded focus:ring-blue-500"
               />
               <label htmlFor="remember-me" className="ml-2 text-sm">
-                Recuérdame
+                {languageData.login.rememberMe}
               </label>
             </div>
             <span className="text-sm cursor-pointer text-text_secondary font-bold">
-              ¿Olvidaste tu contraseña?
+              {languageData.login.forgotPassword}
             </span>
           </div>
           <button
             type="submit"
             className="w-full py-2 text-sm font-medium text-white bg-gradient-button rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-secondary-500"
           >
-            Iniciar Sesión
+            {languageData.login.loginButton}
           </button>
         </form>
 
         <div className="flex items-center py-6">
           <hr className="w-1/2 border-gray-200" />
-          <span className="px-2 text-sm">O</span>
+          <span className="px-2 text-sm">{languageData.login.or}</span>
           <hr className="w-1/2 border-gray-200" />
         </div>
 
@@ -163,7 +172,7 @@ const Login = () => {
               alt="Google"
               className="mr-2 w-5 h-5"
             />
-            <span>Continuar con Google</span>
+            <span>{languageData.login.googleSignIn}</span>
           </button>
           <button
             type="button"
@@ -174,7 +183,7 @@ const Login = () => {
               alt="Facebook"
               className="mr-2 w-5 h-5"
             />
-            <span>Continuar con Facebook</span>
+            <span>{languageData.login.facebookSignIn}</span>
           </button>
         </div>
       </section>
