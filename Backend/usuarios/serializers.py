fb0943c479f6cf7030a3b15ea216c9fc55f9c158
Fernando <fer.gab.sua@gmail.com>
2024-08-03 19:42:35 -0300

    Update serializers.py

--- Backend/usuarios/serializers.py
+++ Backend/usuarios/serializers.py
@@ -14,14 +14,16 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['username', 'email', "is_active"]
+        fields = ['email', "is_active"]
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
         user = User(**validated_data)
+        email = validated_data['email']
+        user.username = email
         new_password = uuid4().hex
         user.set_password(new_password)
         registration_mail(user.email, new_password)
-        user.is_active = False
+        user.is_active = True
         user.save()
         return user
