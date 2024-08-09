764fd942037e711c89eaa647500848e677029ef9
Fernando <fer.gab.sua@gmail.com>
2024-08-09 01:14:03 -0300

    Update production.py

--- Backend/justina_io_server/settings/production.py
+++ Backend/justina_io_server/settings/production.py
@@ -29,7 +29,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:8080',
     'http://127.0.0.1',
     'http://0.0.0.0',
-    'https://justina-latam.vercel.app/',
+    'https://justina-latam.vercel.app',
 ]
 
 
